e9134a09a99e843a0fcf69c33b893df543b4a374
skwasim777 <wasimsk6061@gmail.com>
2023-09-05 17:03:37 +0530

    start to build user management project

---
+++ User_Management_App_Spring_Boot/src/main/java/com/JhonSnow/UserManagementAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.JhonSnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementAppSpringBootApplication.class, args);
+	}
+
+}
---
+++ User_Management_App_Spring_Boot/src/test/java/com/JhonSnow/UserManagementAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JhonSnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementAppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
